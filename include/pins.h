817a6baba0602398ef831584d6ebdeebf8b38632
Ondra Červenka <gogo.lejzr@gmail.com>
2024-01-20 22:49:19 +0100

    alt pin definitions

--- include/pins.h
+++ include/pins.h
@@ -10,9 +10,18 @@
 
 namespace esp32pins
 {
-    constexpr uint8_t kI2CSdaPin{8};
-    constexpr uint8_t kI2CSclPin{10};
+    // LaskaKit ESP32-C3-LPKit board pin definitions.
+    // constexpr uint8_t kI2CSdaPin{8};
+    // constexpr uint8_t kI2CSclPin{10};
 
     // Switch for turning device on and off (sleep mode).
-    constexpr uint8_t kOnOffSwitchPin{6};
+    // constexpr uint8_t kOnOffSwitchPin{6};
+
+    // LaskaKit ESP32 LPKit board pin definitions.
+    constexpr uint8_t kI2CSdaPin{21};
+    constexpr uint8_t kI2CSclPin{22};
+
+    // Switch for turning device on and off (sleep mode).
+    constexpr uint8_t kOnOffSwitchPin{32};
+
 } // namespace esp32pins
