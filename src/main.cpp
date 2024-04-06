4e4199e542c0d59b33180143595f9aa46b19a2d6
Ondra Červenka <gogo.lejzr@gmail.com>
2024-04-06 22:41:27 +0200

    nextion rework

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
 #include <ArduinoJson.h>
-#include <LiquidCrystal_PCF8574.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include <Wire.h>
@@ -11,7 +10,6 @@
 #include "config.h"
 #include "trucksim_topics.h"
 
-LiquidCrystal_PCF8574 lcd(0x27);
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
@@ -23,39 +21,6 @@ uint32_t current_time{0};
 uint32_t current_eta{0};
 // Current rest stop time
 uint32_t current_rest_stop{0};
-
-// Map pin char used for distance display.
-byte char_mappin[] = {
-    B00000,
-    B01110,
-    B11111,
-    B11111,
-    B11111,
-    B01110,
-    B00100,
-    B00100};
-
-// Stopwatch char used for ETA display.
-byte char_stopwatch[] = {
-    B00000,
-    B01110,
-    B10101,
-    B10101,
-    B10101,
-    B10011,
-    B10001,
-    B01110};
-
-// Mug char used for rest stop display.
-byte char_mug[] = {
-    B01010,
-    B00101,
-    B00000,
-    B01111,
-    B11111,
-    B10111,
-    B11111,
-    B01111};
 
 const char *weekdays[] = {
     "MON",
@@ -71,33 +36,10 @@ const char *weekdays[] = {
  */
 void waiting_screen()
 {
-  lcd.clear();
-  // use second row for this message
-  // this is a workaround, so when the board restarts and no game info message is recieved
-  // it can go directly to the displaying telemetry
-  // if the first row is used, the game time message from telemetry will overlap with WiFi connected message
-  lcd.setCursor(0, 1);
-  lcd.print("WiFi connected...");
-  lcd.setCursor(0, 2);
-  lcd.print("Waiting for game...");
-}
-
-/**
- * Screen displayed when game is started (recieved non-null on trucksim/gameinfo topic).
- */
-void welcome_screen(const char *game_id, unsigned int game_version)
-{
-  lcd.clear();
-  lcd.print("TruckSim Clock");
-  lcd.setCursor(0, 1);
-
-  if (game_id != nullptr)
-    lcd.printf("Game: %s", game_id);
-  else
-    lcd.print("Game: unknown");
-
-  lcd.setCursor(0, 2);
-  lcd.printf("Version: %d", game_version);
+  Serial2.write("page 0");
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
 }
 
 /**
@@ -124,30 +66,34 @@ void display_current_time(uint32_t seconds)
   char time_str[16];
   seconds_to_hhmmdd(seconds, time_str);
 
-  lcd.setCursor(0, 0);
-  lcd.print(time_str);
+  Serial2.write("clocl.txt=\"");
+  Serial2.write(time_str);
+  Serial2.write("\"");
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
 }
 
 /**
- * Display given amount of time (in seconds) as a time until some event, e.g. ETA or rest stop.
- * Row and col specify the position on the screen, label is a single character to display before the formated time.
+ * Formats given amount of time (in seconds) as a time until some event, e.g. ETA or rest stop.
  *
  * current_time global variable (containing current game time in seconds) is used to calculate the time when the event will happen.
  *
- * E. g.: P: 01h 23m MON 12:34
+ * Outbuffer has to be at least 20 characters long.
+ * E. g.: Tue 13:30 (01h 23m)
  */
-void display_time_until(uint32_t seconds, uint8_t row, uint8_t col, char label)
+void seconds_to_time_until(uint32_t seconds, char *out_buffer)
 {
-  uint32_t hours = 0, minutes = 0;
-  seconds_to_hours_minutes(seconds, hours, minutes);
+  uint32_t hours_eta = 0, minutes_eta = 0;
+  seconds_to_hours_minutes(seconds, hours_eta, minutes_eta);
 
   // buffer for time string in weekday hh:mm format, filled
   // by seconds_to_hhmmdd function
-  char time_str[16];
-  seconds_to_hhmmdd(seconds + current_time, time_str);
+  uint8_t weekday_num = (seconds / (60 * 60 * 24)) % 7;
+  uint32_t hours = (seconds / 3600) % 24;
+  uint32_t minutes = (seconds / 60) % 60;
 
-  lcd.setCursor(col, row);
-  lcd.printf("%c: %02uh %02um %s", label, hours, minutes, time_str);
+  snprintf(out_buffer, 20, "%s %02u:%02u (%02uh %02um)", weekdays[weekday_num], hours, minutes, hours_eta, minutes_eta);
 }
 
 /**
@@ -155,7 +101,15 @@ void display_time_until(uint32_t seconds, uint8_t row, uint8_t col, char label)
  */
 void display_eta(uint32_t seconds)
 {
-  display_time_until(seconds, 2, 0, '\x02');
+  char eta_str[20];
+  seconds_to_time_until(seconds, eta_str);
+
+  Serial2.write("eta.txt=\"");
+  Serial2.write(eta_str);
+  Serial2.write("\"");
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
 }
 
 /**
@@ -163,7 +117,15 @@ void display_eta(uint32_t seconds)
  */
 void display_rest_stop(uint32_t seconds)
 {
-  display_time_until(seconds, 3, 0, '\x03');
+  char rest_stop_str[20];
+  seconds_to_time_until(seconds, rest_stop_str);
+
+  Serial2.write("rest.txt=\"");
+  Serial2.write(rest_stop_str);
+  Serial2.write("\"");
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
 }
 
 /**
@@ -171,8 +133,14 @@ void display_rest_stop(uint32_t seconds)
  */
 void display_distance(float_t distance)
 {
-  lcd.setCursor(0, 1);
-  lcd.printf("\x01: %14.0f km", distance);
+  char distance_str[20];
+  snprintf(distance_str, 20, "\x01: %14.0f km", distance);
+  Serial2.write("dist.txt=\"");
+  Serial2.write(distance_str);
+  Serial2.write("\"");
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
 }
 
 /**
@@ -180,20 +148,10 @@ void display_distance(float_t distance)
  */
 void default_telemetry_screen()
 {
-  lcd.clear();
   display_current_time(0);
   display_distance(0);
   display_eta(0);
   display_rest_stop(0);
-}
-
-/**
- * Screen displayed when game is exited (null on trucksim/gameinfo topic).
- */
-void goodbye_screen()
-{
-  lcd.clear();
-  lcd.print("Goodbye...");
 }
 
 void callback(char *topic, byte *payload, unsigned int length)
@@ -212,16 +170,9 @@ void callback(char *topic, byte *payload, unsigned int length)
     const char *game_id = doc["game_id"];
 
     if (game_id == nullptr)
-      goodbye_screen();
+      waiting_screen();
     else
     {
-      // Briefly display welcome screen, then switch to the telemetry screen.
-      welcome_screen(game_id, game_version);
-      // This delay means that processing this topic will block main thread for 3 seconds.
-      // If another message is recieved in this time, it will be processed after this delay
-      // This should not interfere with program function however.
-      delay(3000);
-      // Display telemetry screen with default values.
       default_telemetry_screen();
     }
 
@@ -291,18 +242,12 @@ void mqtt_reconnect()
 
 void setup()
 {
+  // nextion is connected to serial2
+  Serial2.begin(115200);
+
   Wire.begin(esp32pins::kI2CSdaPin, esp32pins::kI2CSclPin);
 
-  lcd.begin(20, 4);
-  lcd.setBacklight(255);
-
   WiFi.begin(kWifiSSID, kWifiPassword);
-
-  lcd.print("Connecting to WiFi");
-
-  lcd.createChar(1, char_mappin);    // use \x01 to display this char
-  lcd.createChar(2, char_stopwatch); // use \x02 to display this char
-  lcd.createChar(3, char_mug);       // use \x03 to display this char
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -323,11 +268,6 @@ void loop()
   {
     mqtt_reconnect();
   }
-
-  if (digitalRead(esp32pins::kOnOffSwitchPin) == HIGH)
-    lcd.setBacklight(0);
-  else
-    lcd.setBacklight(255);
 
   mqtt_client.loop();
 }
